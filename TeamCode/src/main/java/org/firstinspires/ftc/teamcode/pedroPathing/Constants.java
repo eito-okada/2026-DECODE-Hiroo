0803342d4d5ce2b368b0699cc87a233c5b915797
a_eracero <aeraceromagic@gmail.com>
2026-01-29 17:20:06 +0900

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -4,17 +4,15 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
-import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.ftc.localization.constants.TwoWheelConstants; // Changed to TwoWheel
+import com.pedropathing.localization.Encoder;
 import com.pedropathing.paths.PathConstraints;
-import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot; // Required for IMU
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
 public class Constants {
 
-    // FIX: Added .localizerType(LocalizerType.PINPOINT
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(7);
 
@@ -29,21 +27,37 @@ public class Constants {
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
-    public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(-182/2.54)
-            .strafePodX(-196/2.54)
-            .distanceUnit(DistanceUnit.MM)
-            .hardwareMapName("pinpoint")
-            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+    // FIX: Using TwoWheelConstants for 2 pods + IMU
+    public static TwoWheelConstants localizerConstants = new TwoWheelConstants()
+            // 1. HARDWARE NAMES (From Driver Station Config)
+            .forwardEncoderName("motor0") // The parallel pod port
+            .strafeEncoderName("motor3")  // The perpendicular pod port
+            .imuName("imu")               // The built-in IMU name
+
+            // 2. DIRECTIONS (Run LocalizationTest to check)
+            .forwardEncoderDirection(Encoder.FORWARD)
+            .strafeEncoderDirection(Encoder.FORWARD)
+
+            // 3. IMU ORIENTATION (Crucial for 2-wheel!)
+            // Check how your Control Hub is mounted on the robot
+            .imuLogoDirection(RevHubOrientationOnRobot.LogoFacingDirection.UP)
+            .imuUsbDirection(RevHubOrientationOnRobot.UsbFacingDirection.FORWARD)
+
+            // 4. PHYSICAL OFFSETS (Inches from center of robot)
+            // forwardPodY: Distance Left(+) or Right(-) of center
+            .forwardPodY(5.5)
+            // strafePodX: Distance Forward(+) or Backward(-) of center
+            .strafePodX(-2.5)
+
+            // 5. TUNING VALUES (Run ForwardTuner and StrafeTuner)
+            .forwardTicksToInches(0.0005)
+            .strafeTicksToInches(0.0005);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
-    // ONLY use this if you cannot update the library
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
-                .pinpointLocalizer(localizerConstants)
+                .twoWheelLocalizer(localizerConstants) // FIX: Changed to twoWheelLocalizer
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
                 .build();
